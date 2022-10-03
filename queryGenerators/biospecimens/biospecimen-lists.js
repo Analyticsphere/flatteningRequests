06976f98a85c9501f29093ccdf361d01eac52736
jacobmpeters <jcbptrs@gmail.com>
2022-10-03 17:21:03 -0400

    Added biospecimenQueryGenerator.js

---
+++ queryGenerators/biospecimens/biospecimen-lists.js
@@ -0,0 +1,8 @@
+const pathToConceptIdList = {
+// Example:
+//  'D_716117817.D_831438612.D_831438612': [
+//    863839736, 531671855, 525375987, 995797773, 431441193, 834822910
+//  ],
+};
+
+module.exports = pathToConceptIdList;
