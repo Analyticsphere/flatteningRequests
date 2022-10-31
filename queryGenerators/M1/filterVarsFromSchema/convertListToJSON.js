7c4ae0a83610b53a29b87a636fea6f87ffeebc34
jacobmpeters <jcbptrs@gmail.com>
2022-10-31 16:57:24 -0400

    regenerated M1V1 using M1V1-variables-auto-new.csv

--- queryGenerators/M1/filterVarsFromSchema/convertListToJSON.js
+++ queryGenerators/M1/filterVarsFromSchema/convertListToJSON.js
@@ -2,13 +2,13 @@
 const fs = require('fs')
 
 // Load *-lists.js file as object
-const pathToConceptIdList = require('./M1-lists');
+const pathToConceptIdList = require('./M1V2-lists');
 
 // pretty-print JSON object to string
 const jsonData = JSON.stringify(pathToConceptIdList, null, 4);
 
 // write JSON string to a file
-fs.writeFile('M1-lists.json', jsonData, err => {
+fs.writeFile('M1V2-lists.json', jsonData, err => {
   if (err) {
     throw err
   }
