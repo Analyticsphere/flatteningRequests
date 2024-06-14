05ca2f1cfa8ea745bf3ae8fe692b26735cd7234a
Jake Peters <jcbptrs@gmail.com>
2024-06-14 11:40:07 -0400

    corrected documentation

--- r/get_bq_table_schema.R
+++ r/get_bq_table_schema.R
@@ -1,41 +1,41 @@
+#' Get the schema of a BigQuery table and optionally save it as JSON
+#'
+#' This function retrieves the schema of a BigQuery table in a specified Google Cloud project, dataset, and table.
+#'
+#' @param project_id A string representing the Google Cloud project ID.
+#' @param dataset_id A string representing the BigQuery dataset ID.
+#' @param table_id A string representing the BigQuery table ID.
+#' @param as_json A logical value indicating whether to return the schema as JSON (default) or as a list.
+#'
+#' @return The schema of the specified BigQuery table as either a JSON string or a list, depending on the 'as_json' parameter.
+#'
+#' @examples
+#'
+#' project_id <- "your-project-id"
+#' dataset_id <- "your-dataset-id"
+#' table_id   <- "your-table-id"
+#'
+#' # Retrieve schema as JSON and save it to a file
+#' schema_json <- get_bq_table_schema(project_id, dataset_id, table_id)
+#' writeLines(schema_json, "schema.json")
+#'
+#' # Retrieve schema as a list
+#' schema_list <- get_bq_table_schema(project_id, dataset_id, table_id, as_json = FALSE)
+#' write(schema, file = 'schema.json')
+#'
+#' @import jsonlite
+#' @import bigrquery
 get_bq_table_schema <- function(project_id, dataset_id, table_id, as_json = TRUE) {
-  #' Get the schema of a BigQuery table and optionally save it as JSON
-  #'
-  #' This function retrieves the schema of a BigQuery table in a specified Google Cloud project, dataset, and table.
-  #'
-  #' @param project_id A string representing the Google Cloud project ID.
-  #' @param dataset_id A string representing the BigQuery dataset ID.
-  #' @param table_id A string representing the BigQuery table ID.
-  #' @param as_json A logical value indicating whether to return the schema as JSON (default) or as a list.
-  #'
-  #' @return The schema of the specified BigQuery table as either a JSON string or a list, depending on the 'as_json' parameter.
-  #'
-  #' @examples
-  #'
-  #' project_id <- "your-project-id"
-  #' dataset_id <- "your-dataset-id"
-  #' table_id   <- "your-table-id"
-  #'
-  #' # Retrieve schema as JSON and save it to a file
-  #' schema_json <- get_bq_table_schema(project_id, dataset_id, table_id)
-  #' writeLines(schema_json, "schema.json")
-  #'
-  #' # Retrieve schema as a list
-  #' schema_list <- get_bq_table_schema(project_id, dataset_id, table_id, as_json = FALSE)
-  #' write(schema, file = 'schema.json')
-  #'
-  #' @import jsonlite
-  #' @import bigrquery
-  
-  # Construct BQ table object
+
+    # Construct BQ table object
   bq_table   <- bq_table(project_id, dataset_id, table_id) 
   
   # Get "fields"
   fields <- bigrquery::bq_table_fields(bq_table)
-  head(fields)
+  # head(fields)
   
   # Get table "metadata" and "schema"
-  schema <- bigrquery::bq_table_meta(bq_table, fields = schema)
+  schema          <- bigrquery::bq_table_meta(bq_table, fields = schema)
   schema          <- metadata$schema
   metadata$schema <- NULL # remove the schema from this list
   # head(metadata)
